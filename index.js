172b029e446a070f59453cbbbffbad979ba8b17a
Gilbert Torchon <gilbert@noukod.com>
2019-02-08 15:17:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function driversByRevenue(drivers){
 
 function driversByName(drivers){
   return drivers.slice().sort(function(a,b){
-    return a.localeCompare(b)
+    return a.localeCompare(b);
   })
 }
+
